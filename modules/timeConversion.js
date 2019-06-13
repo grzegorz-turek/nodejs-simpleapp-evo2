5f69df58c79d85171ba472a22bd3005146a51626
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-06-13 11:45:38 +0200

    Final v.5

--- modules/timeConversion.js
+++ modules/timeConversion.js
@@ -1,11 +1,11 @@
 'use strict';
 var os = require('os');
-var uptime = os.uptime();
-var fullHours = Math.floor(uptime / 3600);
-var fullMinutes = Math.floor((uptime - (fullHours * 3600)) / 60);
-var fullSeconds = uptime - (fullHours * 3600) - (fullMinutes * 60);
 
 function timeDisplay () {
+    var uptime = os.uptime();
+    var fullHours = Math.floor(uptime / 3600);
+    var fullMinutes = Math.floor((uptime - (fullHours * 3600)) / 60);
+    var fullSeconds = uptime - (fullHours * 3600) - (fullMinutes * 60);
     console.log('Time from system start: ' + fullHours + ' hrs ' + fullMinutes + ' min ' + fullSeconds + ' sec');
 }
 
